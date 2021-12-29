e9d2cc2faa4f54087091e78641c924194411078d
yangliu <yangliulnn@gmail.com>
2021-12-29 22:11:36 +0800

    0.1.2

--- contracts/periphery/examples/ExampleFlashSwap.sol
+++ contracts/periphery/examples/ExampleFlashSwap.sol
@@ -1,6 +1,6 @@
 pragma solidity =0.6.6;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Callee.sol';
+import '../../core/interfaces/IUniswapV2Callee.sol';
 
 import '../libraries/UniswapV2Library.sol';
 import '../interfaces/V1/IUniswapV1Factory.sol';
